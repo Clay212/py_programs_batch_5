73a0e69f9eb144c4d9d22f112f7fdd0c06df3cdf
Clay212 <djohnlaei@gmail.com>
2025-03-20 21:55:52 +0800

    six digits

--- six_digit_number.py
+++ six_digit_number.py
@@ -1,2 +1,8 @@
 # Ask the user to input a number
 num = int(input("Enter a number (0-1000): "))
+if 0 <= num <= 1000:
+    #Format the input in six digits
+    digit = f"{num:06d}" 
+    print("Formatted Output:", digit)
+else:
+    print("Invalid input! Please enter a number between 0 and 1000 :) .") 
